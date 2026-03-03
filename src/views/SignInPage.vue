af2edf4bc8cefa4c14ef0b2f86ea40e3a644ce62
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-03-03 16:13:53 -0500

    lesson finished

--- src/views/SignInPage.vue
+++ src/views/SignInPage.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue'
 import { useFirebaseAuth } from 'vuefire'
-import { createUserWithEmailAndPassword } from '@firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth'
 
 import BaseButton from '@/components/base/BaseButton.vue'
 import BaseContainer from '@/components/base/BaseContainer.vue'
@@ -9,7 +9,7 @@ import BaseCard from '@/components/base/BaseCard.vue'
 import BaseForm from '@/components/base/BaseForm.vue'
 import BaseInput from '@/components/base/BaseInput.vue'
 
-const newUser = ref({
+const userInput = ref({
   email: '',
   password: '',
 })
@@ -19,8 +19,27 @@ const auth = useFirebaseAuth()
 async function createUser() {
   createUserWithEmailAndPassword(
     auth,
-    newUser.value.email,
-    newUser.value.password
+    userInput.value.email,
+    userInput.value.password
+  )
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user
+      console.log(user)
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+      // ..
+    })
+}
+
+async function signInToFirebase() {
+  signInWithEmailAndPassword(
+    auth,
+    userInput.value.email,
+    userInput.value.password
   )
     .then((userCredential) => {
       // Signed in
@@ -42,29 +61,14 @@ async function createUser() {
     <BaseCard>
       <template v-slot:default>
         <BaseForm>
-          <BaseInput
-            v-model="newUser.email"
-            type="email"
-            label="Email"
-            required
-            placeholder="eleanorshellstrop@thegoodplace.com"
-          />
-          <BaseInput
-            v-model="newUser.password"
-            label="Password"
-            type="password"
-            required
-          />
+          <BaseInput v-model="userInput.email" type="email" label="Email" required
+            placeholder="eleanorshellstrop@thegoodplace.com" />
+          <BaseInput v-model="userInput.password" label="Password" type="password" required />
         </BaseForm>
       </template>
       <template v-slot:actions>
-        <BaseButton variant="tonal" color="success"> Sign In </BaseButton>
-        <BaseButton
-          @click="createUser"
-          variant="tonal"
-          color="secondary"
-          outline
-        >
+        <BaseButton @click="signInToFirebase" variant="tonal" color="success"> Sign In </BaseButton>
+        <BaseButton @click="createUser" variant="tonal" color="secondary" outline>
           Create New User
         </BaseButton>
       </template>
