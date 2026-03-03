8ad5e14eacc95a94cf20774a100764937c4c9fc3
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-03-03 15:36:07 -0500

    lesson finished

--- src/views/SignInPage.vue
+++ src/views/SignInPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue'
-
+import { useFirebaseAuth } from 'vuefire'
+import { createUserWithEmailAndPassword } from '@firebase/auth'
 import BaseButton from '@/components/base/BaseButton.vue'
 import BaseContainer from '@/components/base/BaseContainer.vue'
 import BaseCard from '@/components/base/BaseCard.vue'
@@ -11,6 +12,23 @@ const newUser = ref({
   email: '',
   password: '',
 })
+
+const auth = useFirebaseAuth();
+
+async function createUser() {
+  createUserWithEmailAndPassword(
+    auth,
+    newUser.value.email,
+    newUser.value.password
+  )
+    .then((userCredential) => {
+      const user = userCredential.user
+    })
+    .catch((error) => {
+      const errorCode = error.code
+      const errorMessage = error.message
+    })
+}
 </script>
 
 <template>
@@ -19,24 +37,14 @@ const newUser = ref({
     <BaseCard>
       <template v-slot:default>
         <BaseForm>
-          <BaseInput
-            v-model="newUser.email"
-            type="email"
-            label="Email"
-            required
-            placeholder="eleanorshellstrop@thegoodplace.com"
-          />
-          <BaseInput
-            v-model="newUser.password"
-            label="Password"
-            type="password"
-            required
-          />
+          <BaseInput v-model="newUser.email" type="email" label="Email" required
+            placeholder="eleanorshellstrop@thegoodplace.com" />
+          <BaseInput v-model="newUser.password" label="Password" type="password" required />
         </BaseForm>
       </template>
       <template v-slot:actions>
         <BaseButton variant="tonal" color="success"> Sign In </BaseButton>
-        <BaseButton variant="tonal" color="secondary" outline>
+        <BaseButton @click="createUser" variant="tonal" color="secondary" outline>
           Create New User
         </BaseButton>
       </template>
