37acc101491ba9cc4bf4eec82f672d5ce4b0ec39
Jacky Southiphonh <southiphonhjacky@gmail.com>
2026-03-03 15:10:04 -0500

    lesson finished

--- src/components/CafeCard.vue
+++ src/components/CafeCard.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { computed } from 'vue'
+import { useFirestore } from 'vuefire'
+import { deleteDoc, doc } from 'firebase/firestore'
 import BaseButton from '@/components/base/BaseButton.vue'
 import BaseIcon from '@/components/base/BaseIcon.vue'
 import BaseImageCard from '@/components/base/BaseImageCard.vue'
 import BaseRating from '@/components/base/BaseRating.vue'
 import CafeImage from '@/components/CafeImage.vue'
 
+const db = useFirestore()
 const props = defineProps({
   description: {
     type: String,
@@ -51,6 +54,10 @@ const priceSymbol = computed(() => {
       return 'No price defined'
   }
 })
+
+async function deleteCafe() {
+  await deleteDoc(doc(db, 'cafes', props.docId))
+}
 </script>
 
 <template>
@@ -91,7 +98,7 @@ const priceSymbol = computed(() => {
       <BaseButton color="primary" :to="`/cafe/${docId}`">
         <BaseIcon icon="mdi-pencil" class="mr-1" /> Edit
       </BaseButton>
-      <BaseButton color="error" text disabled>
+      <BaseButton @click="deleteCafe" color="error" text>
         <BaseIcon icon="mdi-trash-can-outline" class="mr-1" />
         Delete
       </BaseButton>
